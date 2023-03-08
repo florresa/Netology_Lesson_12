21fcb1e7894a980fed0f3c24615fff98a8847301
Olga <olakostina31@gmail.com>
2023-03-08 13:31:48 +0300

    Added changes according to the task

---
+++ src/main/java/org/example/ProductRepository.java
@@ -0,0 +1,29 @@
+package org.example;
+
+public class ProductRepository {
+    private Product[] items = new Product[0];
+
+    public void save(Product product) {
+        Product[] tmp = new Product[items.length + 1];
+        System.arraycopy(items, 0, tmp, 0, items.length);
+        tmp[tmp.length - 1] = product;
+        items = tmp;
+    }
+
+    public void removeById(int id) {
+        Product[] tmp = new Product[items.length - 1];
+        int copyToIndex = 0;
+        for (Product product : items) {
+            if (product.getId() != id) {
+                tmp[copyToIndex] = product;
+                copyToIndex++;
+            }
+        }
+        items = tmp;
+    }
+
+    public Product[] findAll() {
+        return items;
+    }
+}
+
